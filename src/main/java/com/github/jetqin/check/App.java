50db1eed81e7a639a80bcf4aee34528631f0b578
JetQin <qinjet@gmail.com>
2016-02-25 22:08:41 +0800

    initialize java static check project

---
+++ src/main/java/com/github/jetqin/check/App.java
@@ -0,0 +1,13 @@
+package com.github.jetqin.check;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
