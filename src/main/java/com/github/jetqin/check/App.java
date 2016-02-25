c9bfa793f86818363ab0570192554e8869c52e97
JetQin <qinjet@gmail.com>
2016-02-26 00:43:30 +0800

    add static check configuration

--- src/main/java/com/github/jetqin/check/App.java
+++ src/main/java/com/github/jetqin/check/App.java
@@ -1,13 +1,23 @@
 package com.github.jetqin.check;
 
+import java.util.*;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	public final static String asssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss = "123";
+			
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+    
+    public void test()
+    {
+//    	int as ;
+    	
     }
 }
