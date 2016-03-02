00e7ec0fc5bcd83a04ac72b464ce908c828cdf5c
JetQin <qinjet@gmail.com>
2016-03-02 18:04:49 +0800

    add check style format

--- src/main/java/com/github/jetqin/check/App.java
+++ src/main/java/com/github/jetqin/check/App.java
@@ -1,23 +1,52 @@
 package com.github.jetqin.check;
 
+import java.lang.annotation.Target;
 import java.util.*;
+
+import javax.annotation.Resource;
 
 /**
  * Hello world!
  *
  */
+@Deprecated
+@Resource
 public class App 
 {
 	public final static String asssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss = "123";
 			
-    public static void main( String[] args )
-    {
+	public String nameConvertionParameter ;
+	
+  public static void main( String[] args )
+  {
         System.out.println( "Hello World!" );
-    }
-    
+  }
+  
+  @Deprecated
+  @Resource
     public void test()
     {
 //    	int as ;
+    
+    int index = 5;
+      while(index < 10)
+         System.out.println("Hello");index++;
     	
     }
+  
+  public void testApp()
+  {
+    int key = 2;
+     switch (key)
+    {
+      case 2:
+        System.out.println(2);
+      case 3:
+        ;
+      case 4:
+        System.out.println(4);
+        break;
+        
+    }
+  }
 }
--- src/test/java/com/github/jetqin/check/AppTest.java
+++ src/test/java/com/github/jetqin/check/AppTest.java
@@ -7,8 +7,7 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest  extends TestCase
 {
     /**
      * Create the test case
@@ -31,8 +30,5 @@ public class AppTest
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+  
 }
